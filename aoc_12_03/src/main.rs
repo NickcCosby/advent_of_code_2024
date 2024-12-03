163584832985588bb54eb049b7423cdeca4f93a0
Nick <nickccosby@gmail.com>
2024-12-03 13:01:06 -0800

    refactored puzzle 2 logic

--- aoc_12_03/src/main.rs
+++ aoc_12_03/src/main.rs
@@ -26,20 +26,36 @@ fn puzzle_2() {
 
     let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)").unwrap();
 
-    let mut acc: i32 = 0;
-    let mut mul_on = true;
+    // let mut acc: i32 = 0;
+    // let mut mul_on = true;
 
-    for capture in re.captures_iter(&input) {
-        if capture.get(0).unwrap().as_str() == "do()" {
-            mul_on = true;
-        } else if capture.get(0).unwrap().as_str() == "don't()" {
-            mul_on = false;
-        } else if mul_on {
-            let parse = |group: Option<Match>| group.unwrap().as_str().parse::<i32>().unwrap();
-            let result = parse(capture.get(1)) * parse(capture.get(2));
-            acc += result;
-        }
-    }
+    // for capture in re.captures_iter(&input) {
+    //     if capture.get(0).unwrap().as_str() == "do()" {
+    //         mul_on = true;
+    //     } else if capture.get(0).unwrap().as_str() == "don't()" {
+    //         mul_on = false;
+    //     } else if mul_on {
+    //         let parse = |group: Option<Match>| group.unwrap().as_str().parse::<i32>().unwrap();
+    //         let result = parse(capture.get(1)) * parse(capture.get(2));
+    //         acc += result;
+    //     }
+    // }
 
-    dbg!(acc);
+    let (_, out) = re
+        .captures_iter(&input)
+        .fold((true, 0), |(mul_on, acc), capture| {
+            if capture.get(0).unwrap().as_str() == "do()" {
+                (true, acc)
+            } else if capture.get(0).unwrap().as_str() == "don't()" {
+                (false, acc)
+            } else if mul_on {
+                let parse = |group: Option<Match>| group.unwrap().as_str().parse::<i32>().unwrap();
+                let result = parse(capture.get(1)) * parse(capture.get(2));
+                (mul_on, acc + result)
+            } else {
+                (mul_on, acc)
+            }
+        });
+
+    dbg!(out);
 }
